e872783f886615801cd6fc4d593909ac11de0afb
Filip Solich <dev@solich.me>
2026-06-02 20:50:02 +0200

    t

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-source "$(dirname "$0")/vars.sh"
+#source "$(dirname "$0")/vars.sh"
 
 CGO_ENABLED=0 GOOS=linux go build -o bin/mkrepo -trimpath -ldflags="-buildid= -X $MODULE/internal.revision=$REVISION -X $MODULE/internal.version=$VERSION -X $MODULE/internal.buildDatetime=$DATETIME" ./cmd/mkrepo
