955a006958cc3ee9d6599a76c9d4397f215393bf
Filip Solich <dev@solich.me>
2026-06-02 21:00:26 +0200

    test cache

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
-#set -euo pipefail
+set -euo pipefail
 
-#source "$(dirname "$0")/vars.sh"
-MODULE=github.com/mkrepo-dev/mkrepo
+source "$(dirname "$0")/vars.sh"
 
 CGO_ENABLED=0 GOOS=linux go build -o bin/mkrepo -trimpath -ldflags="-buildid= -X $MODULE/internal.revision=$REVISION -X $MODULE/internal.version=$VERSION -X $MODULE/internal.buildDatetime=$DATETIME" ./cmd/mkrepo
